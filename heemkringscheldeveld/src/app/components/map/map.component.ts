cf070113110989c9952a4f10302b734fe98132dd
Maarten Brysbaert <maarten.brysbaert@qite.be>
2021-11-17 22:36:14 +0100

    Added labels to map

--- heemkringscheldeveld/src/app/components/map/map.component.ts
+++ heemkringscheldeveld/src/app/components/map/map.component.ts
@@ -10,6 +10,18 @@ import * as mapboxgl from 'mapbox-gl';
 export class MapComponent implements OnInit {
   private hoveredStateId = null;
 
+  private markers = [
+    { title: "De Pinte", lat: 3.6506400381204003, lng: 50.99312900139441 },
+    { title: "Nazareth", lat: 3.595351587861103, lng: 50.95894464646918 },
+    { title: "Deurle", lat: 3.6098534764536225, lng: 51.0006016923999 },
+    { title: "Sint-Martens-Latem", lat: 3.632059493360572, lng: 51.0157145253028 },
+    { title: "Zevergem", lat: 3.685535207546934, lng: 50.9783512081446 },
+    { title: "Zwijnaarde", lat: 3.704115752306734, lng: 51.00430906945468 },
+    { title: "Afsnee",lat: 3.657437798398405, lng: 51.03281745053306 },
+    { title: "Eke", lat: 3.6497336700839185, lng: 50.96208449293681 },
+    { title: "Sint-Denijs-Westrem", lat: 3.675565159139353, lng: 51.020846065420955 }
+  ]
+
   constructor(private router: Router) {
   }
 
@@ -101,7 +113,7 @@ export class MapComponent implements OnInit {
       });
 
       map.on('click', 'state-fills', e => {
-
+        console.log(e.lngLat.lng, e.lngLat.lat);
         map.flyTo({
           center: [e.lngLat.lng, e.lngLat.lat],
           zoom: 11.5
@@ -109,9 +121,46 @@ export class MapComponent implements OnInit {
 
         const router = this.router;
         const path = e.features[0].properties.path;
-        router.navigate(['gemeente', path]);
+        // router.navigate(['gemeente', path]);
         // setTimeout(() => router.navigate([path]), 500);
-      })
+      });
+
+      const test = {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: this.markers.map(marker => ({
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [marker.lat, marker.lng]
+            },
+            properties: {
+              title: marker.title
+            }
+          }))
+        }
+      };
+
+      map.addSource('points', test);
+      // Add a symbol layer
+      map.addLayer({
+        'id': 'points',
+        'type': 'symbol',
+        'source': 'points',
+        'layout': {
+          'icon-image': 'custom-marker',
+          // get the title name from the source's "title" property
+          'text-field': ['get', 'title'],
+          'text-font': [
+            'Open Sans Semibold',
+            'Arial Unicode MS Bold'
+          ],
+          'text-offset': [0, 0],
+          'text-anchor': 'center',
+          'text-size': 11
+        }
+      });
     });
   }
 }
